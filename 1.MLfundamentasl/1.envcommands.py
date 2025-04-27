215b4d9b0df806782eaf8c01ece8792e1d7ded9d
Reema S <144640434+rsolan@users.noreply.github.com>
2025-04-27 02:36:45 -0400

    Update 1.envcommands.py

--- 1.MLfundamentasl/1.envcommands.py
+++ 1.MLfundamentasl/1.envcommands.py
@@ -54,4 +54,30 @@ pip install -r requirements.txt
 
 
 #4. src - data_ingestion.py
+import numpy as np
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+
+# Load data
+df = pd.read_csv('https://raw.githubusercontent.com/campusx-official/jupyter-masterclass/main/tweet_emotions.csv')
+df.drop(columns=['tweet_id'], inplace=True)
+
+# Filter for happiness and sadness
+final_df = df[df['sentiment'].isin(['happiness', 'sadness'])]
+final_df['sentiment'].replace({'happiness': 1, 'sadness': 0}, inplace=True)
+
+# Split into train and test
+train_data, test_data = train_test_split(final_df, test_size=0.2, random_state=42)
+
+# Save to local folders
+data_path = os.path.join("data", "raw")
+os.makedirs(data_path, exist_ok=True)
+
+train_data.to_csv(os.path.join(data_path, "train.csv"), index=False)
+test_data.to_csv(os.path.join(data_path, "test.csv"), index=False)
+
+# data - raw test/train file created
+
+# 5. cmd - 
 
